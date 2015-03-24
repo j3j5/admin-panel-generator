1018f4bf919fa637bcd6b15247a4ecb25117f8f1
nik <nikolaspalevic@gmail.com>
2015-03-24 14:58:59 +0100

    Route prefix

--- src/routes.php
+++ src/routes.php
@@ -1,8 +1,10 @@
 <?php
 
-Route::get('/admin/{modelName}', 'VivifyIdeas\AdminPanelGenerator\Http\Controllers\MainController@index');
-Route::get('/admin/{modelName}/create', 'VivifyIdeas\AdminPanelGenerator\Http\Controllers\MainController@create');
-Route::get('/admin/{modelName}/edit/{id}', 'VivifyIdeas\AdminPanelGenerator\Http\Controllers\MainController@edit');
-Route::put('/admin/{modelName}/{id}', 'VivifyIdeas\AdminPanelGenerator\Http\Controllers\MainController@update');
-Route::post('/admin/{modelName}', 'VivifyIdeas\AdminPanelGenerator\Http\Controllers\MainController@store');
-Route::get('/admin/{modelName}/delete/{id}', 'VivifyIdeas\AdminPanelGenerator\Http\Controllers\MainController@delete');
+$prefix = packageConfig('prefix');
+
+Route::get($prefix . '/{modelName}', 'VivifyIdeas\AdminPanelGenerator\Http\Controllers\MainController@index');
+Route::get($prefix . '/{modelName}/create', 'VivifyIdeas\AdminPanelGenerator\Http\Controllers\MainController@create');
+Route::get($prefix . '/admin/{modelName}/edit/{id}', 'VivifyIdeas\AdminPanelGenerator\Http\Controllers\MainController@edit');
+Route::put($prefix . '/{modelName}/{id}', 'VivifyIdeas\AdminPanelGenerator\Http\Controllers\MainController@update');
+Route::post($prefix . '/{modelName}', 'VivifyIdeas\AdminPanelGenerator\Http\Controllers\MainController@store');
+Route::get($prefix . '/{modelName}/delete/{id}', 'VivifyIdeas\AdminPanelGenerator\Http\Controllers\MainController@delete');
