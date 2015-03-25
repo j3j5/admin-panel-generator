cccf358348fd7ec1d9208318392e77416c37c423
nik <nikolaspalevic@gmail.com>
2015-03-25 14:25:10 +0100

    Route for edit fixed

--- src/routes.php
+++ src/routes.php
@@ -4,7 +4,7 @@ $prefix = packageConfig('prefix');
 
 Route::get($prefix . '/{modelName}', 'VivifyIdeas\AdminPanelGenerator\Http\Controllers\MainController@index');
 Route::get($prefix . '/{modelName}/create', 'VivifyIdeas\AdminPanelGenerator\Http\Controllers\MainController@create');
-Route::get($prefix . '/admin/{modelName}/edit/{id}', 'VivifyIdeas\AdminPanelGenerator\Http\Controllers\MainController@edit');
+Route::get($prefix . '/{modelName}/edit/{id}', 'VivifyIdeas\AdminPanelGenerator\Http\Controllers\MainController@edit');
 Route::put($prefix . '/{modelName}/{id}', 'VivifyIdeas\AdminPanelGenerator\Http\Controllers\MainController@update');
 Route::post($prefix . '/{modelName}', 'VivifyIdeas\AdminPanelGenerator\Http\Controllers\MainController@store');
 Route::get($prefix . '/{modelName}/delete/{id}', 'VivifyIdeas\AdminPanelGenerator\Http\Controllers\MainController@delete');
