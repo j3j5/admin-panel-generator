658ff9a58b42505f99e0ed9ab4a79e50ab61a231
Julio Foulquie <jfoulquie@gmail.com>
2015-11-15 00:11:47 -0300

    Add language files for English and Spanish.

---
+++ resources/lang/en/vivify.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+	'admin-panel'	=> 'Admin Panel',
+	'create'		=> 'Create',
+	'filter-noun'	=> 'Filter',
+	'filter-verb'	=> 'Filter',
+	'reset'			=> 'Reset',
+	'no-records'	=> 'No records found',
+	'actions'		=> 'Actions',
+	'edit'			=> 'Edit',
+	'confirmation'	=> 'Are you sure that you want to delete this row?',
+	'remove'		=> 'Remove',
+	'insert'		=> 'Insert',
+	'cancel'		=> 'Cancel',
+	'update'		=> 'Update',
+];
